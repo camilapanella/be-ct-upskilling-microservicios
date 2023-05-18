689efcef1ffadf21f58aa337201dd50a7c3adce3
camilapanella <ca10_mimi@hotmail.com>
2023-05-18 15:46:50 -0300

    first commit

---
+++ star_wars_api_microservice/planets/src/routes/index.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const controllers = require("../controllers");
+const middlewares = require("../middlewares")
+
+const router = Router();
+
+router.get("/", controllers.getPlanets);
+
+router.post("/",middlewares.planetValidation, controllers.postPlanets)
+
+module.exports = router;
---
+++ star_wars_api_mono/index.js
@@ -0,0 +1,5 @@
+const server = require("./src/server");
+
+server.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
